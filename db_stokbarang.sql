d5088f9c768ef0dc8b56f3f7ec68991e8dcbb687
izzannazzi <52392575+izzannazzi@users.noreply.github.com>
2022-03-22 22:55:03 +0700

    update sql terbaru

--- db_stokbarang.sql
+++ db_stokbarang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2022 at 07:40 PM
+-- Generation Time: Mar 22, 2022 at 04:53 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -31,8 +31,17 @@ CREATE TABLE `barang` (
   `Id` int(11) NOT NULL,
   `code` varchar(20) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `quantity` decimal(10,0) NOT NULL
+  `quantity` decimal(10,0) NOT NULL,
+  `gambar` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`Id`, `code`, `name`, `quantity`, `gambar`) VALUES
+(1, '87204378391', 'Beras Mentari 5KG', '30', 'berasmentari.jpg'),
+(5, '65487874212', 'Pepsodent', '15', 'pepsodent.jpg');
 
 --
 -- Indexes for dumped tables
@@ -52,7 +61,7 @@ ALTER TABLE `barang`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
